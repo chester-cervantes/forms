8054d749fa61ec3315090965cd1e1af9ba83c3c7
ecbob <chestercervantes13@hotmail.com>
2020-04-01 18:07:15 -0700

    - test ng code - change forms table to table format

--- modules/forms/client/controllers/list-forms.client.controller.js
+++ modules/forms/client/controllers/list-forms.client.controller.js
@@ -5,9 +5,9 @@
     .module('forms')
     .controller('FormsListController', FormsListController);
 
-  FormsListController.$inject = ['FormsService' , '$http'];
+  FormsListController.$inject = ['FormsService' , '$http', '$scope'];
 
-  function FormsListController(FormsService , $http) {
+  function FormsListController(FormsService , $http, $scope) {
     var vm = this;
     vm.forms = {};
 
@@ -21,6 +21,29 @@
       console.log ( "error data = " + data.message ); // DEBUG
     } );
 
+    $scope.Sort1 = function () {
+      console.log('asdasdasds');
+      var forms = document.getElementById('forms');
 
+      function getArrayFromTable(table) {
+        return 'test';
+      }
+
+      var array = getArrayFromTable(forms);
+      forms.innerHTML = array;
+
+      // var row = tbody.insertRow(tbody.rows.length);
+
+      // var location_name = row.insertCell(0);
+      // var date_time = row.insertCell(1);
+      // var weather = row.insertCell(2);
+      // var temperature = row.insertCell(3);
+      //
+      // location_name.innerHTML = weather_data['name'];
+      // date_time.innerHTML = timeConverter(weather_data['dt']);
+      // weather.innerHTML = weather_data['weather']['0']['main'];
+      // temperature.innerHTML = round(convertKelvinToCelsius(weather_data['main']['temp']), 2);
+    }
   }
+
 }());
