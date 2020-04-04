6778cfc2562bfeffb3d6d54d5a5cdbb70ce37040
Jordan Ho <jordan_ho@sfu.ca>
2020-04-04 13:25:42 -0700

    Revert "Merge branch '29-form-google-map-modal' into 'master'"
    This reverts merge request !25

--- modules/forms/client/controllers/forms.client.controller.js
+++ modules/forms/client/controllers/forms.client.controller.js
@@ -155,11 +155,6 @@
       selectedLocation.value = "";
     }
 
-    vm.recenterLocation = recenterLocation;
-    function recenterLocation() {
-      $scope.goToMyLocation();
-    }
-
 
     $scope.initGoogleMapApi = function (callback) {
       if (!googleMapAPIInit) {
@@ -214,6 +209,12 @@
     // failed.", it means you probably did not give permission for the browser to
     // locate you.
     var map, infoWindow, mapOptions, searchBox;
+
+    // Go to my location button
+    var currLocationBtn = document.getElementById("curr-location-btn");
+    currLocationBtn.addEventListener('click', function () {
+      $scope.goToMyLocation();
+    });
 
     // function to handle recentering of map to current location
     $scope.goToMyLocation = function () {
